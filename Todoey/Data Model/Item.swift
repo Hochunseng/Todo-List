ed718cfe37d65925c70a4fb0034483ada70b5d65
Jason Ho <jasonho@macbook-pro-5.local>
2018-03-02 22:51:17 -0800

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Jason Ho on 3/2/18.
+//  Copyright © 2018 Jason Ho. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title: String = ""
+    var done: Bool = false
+    
+}
